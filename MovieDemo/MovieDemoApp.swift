d9fee00dccf0a09d5713cd7092ed936d6660880b
Sandeep Joshi <24805252+isandeepj@users.noreply.github.com>
2024-02-11 16:15:32 +0530

    Initial Commit

---
+++ MovieDemo/MovieDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDemoApp.swift
+//  MovieDemo
+//
+//  Created by Sandeep on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
